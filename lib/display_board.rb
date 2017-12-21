3868645eea04ae0cc4a3a002b467a04b1dca39ff
Eden Mesfun <emesfun@gmail.com>
2017-12-21 15:29:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 def display_board(board)
 
-  puts "   |   |   "
+  puts " X |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | X |   "
   puts "-----------"
-  puts " O | O | O "
+  puts "   |   | X "
 
 end
-board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+board = ["X", " ", " ", "X", " ", " ", "X", " ", " "]
 display_board(board)
